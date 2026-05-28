6582b2b37a5929ba9f39c3cd19f8b08a2303f5fe
colin <czoesq@gmail.com>
2026-05-28 18:43:46 -0500

    Use collision shape for nav mesh baking instead of nonexistent create_from_mesh_arrays

--- Scripts/Navigation/ground_nav.gd
+++ Scripts/Navigation/ground_nav.gd
@@ -1,6 +1,14 @@
 extends NavigationRegion3D
 
 func _ready() -> void:
+	var src := StaticBody3D.new()
+	var col := CollisionShape3D.new()
+	var box := BoxShape3D.new()
+	box.size = Vector3(20, 0.1, 20)
+	col.shape = box
+	src.add_child(col)
+	add_child(src)
+
 	var nav_mesh := NavigationMesh.new()
 	nav_mesh.cell_size = 0.25
 	nav_mesh.cell_height = 0.25
@@ -8,16 +16,9 @@ func _ready() -> void:
 	nav_mesh.agent_height = 2.0
 
 	var source_geo := NavigationMeshSourceGeometryData3D.new()
-	var arrays := []
-	arrays.resize(Mesh.ARRAY_MAX)
-	arrays[Mesh.ARRAY_VERTEX] = PackedVector3Array([
-		Vector3(-10, 0, -10),
-		Vector3(10, 0, -10),
-		Vector3(-10, 0, 10),
-		Vector3(10, 0, 10),
-	])
-	arrays[Mesh.ARRAY_INDEX] = PackedInt32Array([0, 2, 3, 0, 3, 1])
-	source_geo.create_from_mesh_arrays(arrays)
-
+	NavigationServer3D.parse_source_geometry_data(nav_mesh, source_geo, self)
 	NavigationServer3D.bake_from_source_geometry_data(nav_mesh, source_geo)
 	navigation_mesh = nav_mesh
+
+	remove_child(src)
+	src.queue_free()
